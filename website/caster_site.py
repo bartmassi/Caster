b74167689c74e0daf5d9e26e7457a1f2c9457d88
bartmassi <bartmassi.jr@gmail.com>
2018-06-25 16:44:11 -0400

    Updated main function.

--- website/caster_site.py
+++ website/caster_site.py
@@ -2,29 +2,22 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from code_bin import PodcastDB, Cleaner
+from code_bin import PodcastDB
 import gensim
 from newspaper import Article
+import pickle
 #import sqlite3
 #from sqlalchemy import create_engine
 #from sqlite3 import dbapi2 as sqlite
 
 MAX_CHARACTER_DISPLAY = 80#Maximum number of characters to display in podcast episode title, or podcast title.
+NUM_CHARACTER_DISPLAY = 95#Size of episode title string, including padding.
 
 #%%
-
-#These functions handle user input. 
-#Returns text or title of an article from article URL. 
-
-def checkIfURL(url):
-    return True
 
 #This fetches the article information from the URL using the newspaper package.
 #title_only flag controls whether embeddings are evaluated on entire article body, or just title.
 def generateArticleInput(url,title_only=True):
-    
-    if(not checkIfURL(url)):
-        raise ValueError("'%s' is not a valid URL!" % url)
     
     article = Article(url)
     article.download()
@@ -41,23 +34,22 @@ def generateArticleInput(url,title_only=True):
 #Default mean (sim mean) and standard dev (simstd) are empirical values.
 def determine_similarity(sim_score,simmean=0.5473,simstd=0.1038):
     
-    print(sim_score)
-
     z_score = (sim_score - simmean)/simstd
     
     #Determine appropriate output based on similarity
+    
     if(z_score <= -2):
-        return "Very similar"
+        sim_statement = "Very similar"
     elif(z_score <= -1):
-        return "Somewhat similar"
+        sim_statement = "Somewhat similar"
     elif(z_score > -1 and z_score < 1):
-        return "Not too similar"
+        sim_statement = "Not too similar"
     elif(z_score >= 1):
-        return "Somewhat dissimilar"
+        sim_statement = "Somewhat dissimilar"
     elif(z_score >= 2):
-        return "Very dissimilar"
+        sim_statement = "Very dissimilar"
 
-
+    return sim_statement,-z_score
 #%%
 #Setup the podcast database information
 
@@ -74,9 +66,6 @@ with open(floc+dbname,'rb') as fid:
     podcastdb = PodcastDB.PodcastDB(fid=fid,model=word2vec)
 
 print('Model loaded!')
-
-cleaner = Cleaner.Cleaner()
-
 #%%
 #Render website structures. 
 app = dash.Dash()
@@ -92,59 +81,71 @@ app.title = "Caster"
 #This controls how the button interacts with user input.
 @app.callback(
     dash.dependencies.Output('output-container-button', 'children'),
-    [dash.dependencies.Input('button', 'n_clicks')],
+    [dash.dependencies.Input('button','n_clicks')],
     [dash.dependencies.State('input-box', 'value')])
 
 
 
+#%%
 
 #This function returns the episodes formatted in a pretty(ish) way. 
-def update_output(n_clicks, value):
+#The majority of the code below is to format the output in the appropriate way.
+#The call to the model is at the very top.
+def update_output(n_clicks,value):
     
-    output = podcastdb.search_episodes(generateArticleInput(value),verbose=True)
+    if(value=='https://www.cbinsights.com/research/report/amazon-across-financial-services-fintech/'):
+        with open('cached_output.pkl','rb') as fid:
+           output = pickle.load(fid)
+    else:
+        
+        try:
+            article_text = generateArticleInput(value)
+        except:
+            return('Input must be a link to an article!')
+            
+        output = podcastdb.search_episodes(article_text,verbose=False)
     
     #Format output in appropriate table.
     output_table = []
     for i in range(0,len(output[1])):
-        #Grab podcast title and setup a hyperlink.
-        intermediate_table = []
+        
+        #Grab information about the podcast and its relationship to the input.
         podcast_title = output[0].iloc[i]['collectionName']
         podcast_url = output[0].iloc[i]['feedUrl']
-        sim_score = output[0].iloc[i]['similarity']
-        sim_score_statement = determine_similarity(output[0].iloc[i]['similarity'])
+        sim_score_statement,sim_score = determine_similarity(output[0].iloc[i]['similarity'])
+        
+        #Display info about podcast title & Caster Score
+        intermediate_table = []
         intermediate_table.append(html.Tr([html.Td(dcc.Markdown('**Title**'),style={'align':'left'}),
-                                           html.Td(dcc.Markdown('['+podcast_title[0:MAX_CHARACTER_DISPLAY]+']('+podcast_url+')'),style={'text-align':'left'})
+                                           html.Td(dcc.Markdown('['+podcast_title[0:MAX_CHARACTER_DISPLAY]+']('+podcast_url+')'),style={'align':'left','text-align':'left'})
                                            ]))
         intermediate_table.append(html.Tr([html.Td(dcc.Markdown('**Caster Score**'),style={'text-align':'left'}),
-                                           html.Td(dcc.Markdown('%s to your article (%.2f).' % (sim_score_statement,sim_score)),style={'text-align':'left'})
+                                           html.Td(dcc.Markdown('%s to your article (%.2f).' % (sim_score_statement,sim_score)),style={'align':'left','text-align':'left'})
                                            ]))
-        #Grab ep information
+        
+    #Add episode information to output table.
         for j in range(0,len(output[1][i])):
             
-            episode_title = output[1][i][j].title
-            
-            #Try to get a link to the mp3
+            #Try to pull out title & link to episode. 
             try:
-                episode_url = output[1][i][j].links[1].href 
-                intermediate_table.append(html.Tr([html.Td(dcc.Markdown('**Recommended ep #'+str(j+1)+'**'),style={'align':'left'}),
-                                                  html.Td(dcc.Markdown('['+ episode_title[0:MAX_CHARACTER_DISPLAY] + '...](' + episode_url +')'),style={'align':'left'})
-                                                  ],style={'text-align':'left'}))
+                episode_url= output[1][i][j].links[-1].href
             except:
-                try:
-                    episode_url = output[1][i][j].links[0].href 
-                    intermediate_table.append(html.Tr([html.Td(dcc.Markdown('**Recommended ep #'+str(j+1)+'**'),style={'align':'left'}),
-                                            html.Td(dcc.Markdown('['+ episode_title[0:MAX_CHARACTER_DISPLAY] + '...](' + episode_url +')'),style={'align':'left'})
-                                                  ],style={'text-align':'left'}))                
-                except:
-                    intermediate_table.append(html.Tr([html.Td(dcc.Markdown('**Recommended ep #'+str(j+1)+'**'),style={'align':'left'}),
-                                                  html.Td(episode_title[0:MAX_CHARACTER_DISPLAY],style={'align':'left'})
-                                                  ],style={'text-align':'left'}))         
+                episode_url = 'https://www.google.com'
+            episode_title = output[1][i][j].title[0:MAX_CHARACTER_DISPLAY]
+            padding = ' '*(NUM_CHARACTER_DISPLAY-len(episode_title))
+            
+            #generate table entry
+            intermediate_table.append(html.Tr([html.Td(dcc.Markdown('**Recommended ep #'+str(j+1)+'**'),style={'align':'left','text-align':'left'}),
+                                                       html.Td(dcc.Markdown('['+ episode_title + '...](' + episode_url +')'+padding),style={'align':'left','text-align':'left'})
+                                                  ],style={'text-align':'left','align':'left'}))  
+       
         #Grab artwork and format it with title & episodes
         output_table.append(html.Div([
                         html.Div(
                             html.Img(src=output[0].iloc[i]['artworkUrl600'],style={'width': '100%', 'display': 'inline-block'}),
                                  style={'width': '15%', 'display': 'inline-block','vertical-align':'middle'}),
-                        html.Div(html.Table(intermediate_table,style={'border':'2px solid black','border-radius':'5px','width':'74%'}),style={'width': '74%', 'display': 'inline-block','vertical-align':'middle'})
+                            html.Div(html.Table(intermediate_table,style={'border':'2px solid black','border-radius':'5px','width':'74%','text-align':'left','align':'left'}),
+                                 style={'width': '74%', 'display': 'inline-block','vertical-align':'middle','align':'left','text-align':'left'})
                         ]))
     return output_table
 if __name__ == '__main__':
