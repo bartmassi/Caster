557e639870fbddba1e1ef17743cd7db14086eddd
bartmassi <bartmassi.jr@gmail.com>
2018-07-02 18:51:15 -0400

    Made Caster prettier!

--- website/caster_site.py
+++ website/caster_site.py
@@ -6,6 +6,7 @@ from code_bin import PodcastDB
 import gensim
 from newspaper import Article
 import pickle
+from textwrap import dedent
 #import sqlite3
 #from sqlalchemy import create_engine
 #from sqlite3 import dbapi2 as sqlite
@@ -60,7 +61,7 @@ dbname = 'podcast_df_subset_BIGDATA_REDUCED.pkl'
 #Load up gensim model       
 modelfname = 'GoogleNews-vectors-negative300.bin'
 word2vec = gensim.models.KeyedVectors.load_word2vec_format(floc+modelfname, binary=True)
-
+#word2vec = None
 
 with open(floc+dbname,'rb') as fid:
     podcastdb = PodcastDB.PodcastDB(fid=fid,model=word2vec)
@@ -69,12 +70,23 @@ print('Model loaded!')
 #%%
 #Render website structures. 
 app = dash.Dash()
-app.layout = html.Div([html.Div(dcc.Markdown('**Caster**: link articles to discover podcasts!'),style={'font-size':"32"}),
-    html.Div(dcc.Input(id='input-box', type='text',value=cached_url)),
-    html.Button('Submit', id='button',style={'horizontal-align': 'middle'}),
-    html.Div(id='output-container-button',
-             children='Paste a link to a news article!',style={'horizontal-align': 'left'})
-],style={'textAlign':"left","vertical-align":"left"})
+app.layout = html.Div([
+                html.Div([
+                html.H3(dcc.Markdown('*Caster*'), style={'font-family': 'Caster',
+                'background-color': '#CCCCCC', 'text-align': 'center', 'font-size': 40}),
+                html.H3('Discover podcasts with news articles', style={'font-size': 20, 
+                'font-style': 'italic', 'text-align': 'center'}),
+                ]),
+        
+                html.Div([dcc.Input(id='input-box', type='text',value=cached_url),
+                html.Button('Submit', id='button',style={'horizontal-align': 'middle'})]),
+                html.Div(id='output-container-button',
+                     children='Paste a link to a news article!',style={'horizontal-align': 'left'}),
+                     html.Div(dcc.Markdown(dedent('''
+                     Caster is a discovery system that recommends podcasts based on semantic similarity to news articles. Recommending digital media 
+                     is particularly challenging because there is such high turnover; new articles, videos, and podcasts are released every day. As 
+                     a result, traditional recommendation approaches like collaborative filtering may fail because of the constant "cold start" problem.
+                '''))),html.Div(dcc.Markdown('Caster was developed by [Bart Massi](https://www.linkedin.com/in/bartmassi/)'))],style={'textAlign':"left","vertical-align":"left"})
 server = app.server
 app.title = "Caster"
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/brPBPO.css"})
@@ -95,7 +107,7 @@ app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/brPBPO.css"
 def update_output(n_clicks,value):
     
     if(n_clicks is None):
-        return
+        return 'Paste a link to a news article!'
     
     if(value==cached_url):
         with open('cached_output.pkl','rb') as fid:
